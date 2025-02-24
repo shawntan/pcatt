15abb6e2dbd99a7744d7d108365da2db354f0a82
Shawn Tan <shawntan@ibm.com>
2025-02-24 20:29:06 +0000

    test script.

--- test_greedtok.py
+++ test_greedtok.py
@@ -7,10 +7,12 @@ import torch
 if __name__ == "__main__":
     pattern = r"""'s|'t|'re|'ve|'m|'ll|'d| ?[\p{L}]+|[\p{N}]| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
     batch_size = 1024
+    out_file = './min_word_count-1-max_token_size-20-vocab_size-%d' % 2**16
+    print(out_file)
     tokenize: GreedTok = GreedTok()
     # pat_str = r"""'s|'t|'re|'ve|'m|'ll|'d| ?[\p{L}]+| ?[\p{N}]+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
     # dataset: Dataset = load_dataset("wikitext", name="wikitext-2-raw-v1", split="train")
-    test_text = "In 1984, Winston Smith plays the main protagonist..."
+    test_text = "In 1984, Winston Smith is the main protagonist..."
     splitted = regex.findall(pattern, test_text)
     print("Pattern splits:", splitted)
     dataset: Dataset = load_dataset("Zyphra/dclm-dedup", split="train", num_proc=32)
@@ -28,19 +30,20 @@ if __name__ == "__main__":
             yield batch
 
     tokenize = tokenize.train_new_from_iterator(
-        batch_iterator(dataset, batch_idxs),
+        batch_iterator(dataset, batch_idxs, batch_size=batch_size),
         special_tokens_map={
             "pad_token": "<pad>",
             "unk_token": "<unk>", 
             "eos_token": "<eos>"
         },
         min_word_count=1,
-        max_token_size=60,
+        # max_token_size=60,
+        max_token_size=20,
         vocab_size=2**16,
         pattern=pattern
     )
 
-    tokenize.save_pretrained('./tmp')
+    tokenize.save_pretrained(out_file)
     original_str = "The quick brown fox jumps over the lazy dog.\n"
     # idxs = tokenize(original_str)
     idxs = tokenize([original_str])['input_ids'][0]
